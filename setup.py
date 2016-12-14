19b6d71e17f616bed3566d5615b5938bbfe3a497
Lorenzo <tunedconsulting@gmail.com>
2016-12-14 14:11:20 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='hydrus',
+      version='0.0.1',
+      description='A space-based application for W3C HYDRA Draft',
+      author='W3C HYDRA development group',
+      author_email='public-hydra@w3.org',
+      url='https://github.com/HTTP-APIs/hydrus',
+      packages=['flask==0.11'],
+)
