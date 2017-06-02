f9db15dbb06d3586b7a29dcc9b1c043ebd597f8e
Mec-iS <tunedconsulting@gmail.com>
2017-06-02 21:04:12 +0100

    Improve models (see #9). Add docstrings and packages refactoring and cleanup

--- hydrus/classes.py
+++ hydrus/classes.py
@@ -1,9 +1,12 @@
-"""Definition of all Classes in the SubSystem vocabulary."""
+"""Definition of all Classes in the SubSystem and Spacecraft vocabulary."""
 
 import models
 from sqlalchemy.orm import sessionmaker
 
-
+# TODO @xadahiya:
+# Refactor this part to use a JSON-LD parser, so that RDF classes
+#  can be directly imported from the vocabulary. Add the classes in the
+#  `spacecraft` vocabulary.
 classes = {
     "Spacecraft_Communication": models.Classes(name="Spacecraft_Communication"),
     "Spacecraft_Propulsion": models.Classes(name="Spacecraft_Propulsion"),
@@ -14,6 +17,9 @@ classes = {
     "Spacecraft_Structure": models.Classes(name="Spacecraft_Structure"),
     "Spacecraft_CDH": models.Classes(name="Spacecraft_CDH"),
     "Spacecraft_AODCS": models.Classes(name="Spacecraft_AODCS"),
+    "Spacecraft": models.Classes(name="Spacecraft"),
+    "Subsystem_Spacecraft": models.Classes(name="Subsystem_Spacecraft"),  # all the subsystems types, except detectors (or experiments)
+    "Payload_Spacecraft": models.Classes(name="Payload_Spacecraft")  # Detectors are payload not strictly subssytems
 }
 
 if __name__ == "__main__":
