257cd1375f2413423ab7447354e29416f2fd1738
Akshay Dahiya <xadahiya@gmail.com>
2017-06-22 19:57:22 +0530

    Update entrypoint and entrypoint context generators

--- hydrus/hydraspec/entrypoint_context_generator.py
+++ hydrus/hydraspec/entrypoint_context_generator.py
@@ -1,26 +1,41 @@
 """Genrate EntryPoint Context using server url, item_type."""
 import json
+from hydrus.metadata.subsystem_parsed_classes import parsed_classes
+
+def gen_supported_operation(item_type):
+    ITEM_TYPE = item_type
+    op_template = {
+        ITEM_TYPE.lower(): {
+            "@id": "vocab:EntryPoint/"+ITEM_TYPE,
+            "@type": "@id"
+        }
+    }
+    return op_template
+def gen_supported_ops(parsed_classes):
+    supported_ops = []
+    for class_ in parsed_classes:
+        supported_ops.append(gen_supported_operation(class_["title"]))
+
+    return supported_ops
 
 
-def gen_entrypoint_context(server_url, item_type):
+def gen_entrypoint_context(server_url):
     """Generate context for the EntryPoint."""
     SERVER_URL = server_url
-    ITEM_TYPE = item_type
 
     entrypoint_context_template = {
         "@context": {
             "hydra": "http://www.w3.org/ns/hydra/core#",
             "vocab": SERVER_URL + "/api/vocab#",
             "EntryPoint": "vocab:EntryPoint",
-            ITEM_TYPE.lower(): {
-                "@id": "vocab:EntryPoint/"+ITEM_TYPE,
-                "@type": "@id"
-            }
+            ##Supported Operations will be appended here
         }
     }
-
+    supported_ops = gen_supported_ops(parsed_classes)
+    for op in supported_ops:
+        entrypoint_context_template["@context"][op.keys()[0]] = op[op.keys()[0]]
     return json.dumps(entrypoint_context_template, indent=4)
 
 
 if __name__ == "__main__":
-    print(gen_entrypoint_context("http://hydrus.com/", "Cots"))
+    print(gen_entrypoint_context("http://hydrus.com/"))
--- hydrus/hydraspec/entrypoint_generator.py
+++ hydrus/hydraspec/entrypoint_generator.py
@@ -1,21 +1,39 @@
 """Genrate EntryPoint using server url, item_type."""
 import json
+from hydrus.metadata.subsystem_parsed_classes import parsed_classes
 
 
-def gen_entrypoint(server_url, item_type):
+def gen_supported_operation(item_type):
+    ITEM_TYPE = item_type
+    op_template = {
+        ITEM_TYPE.lower(): "api/%s/" % (ITEM_TYPE)
+    }
+    return op_template
+
+def gen_supported_ops(parsed_classes):
+    supported_ops = []
+    for class_ in parsed_classes:
+        supported_ops.append(gen_supported_operation(class_["title"]))
+
+    return supported_ops
+
+
+def gen_entrypoint(server_url):
     """Generate EntryPoint."""
     SERVER_URL = server_url
-    ITEM_TYPE = item_type
 
     entrypoint_template = {
       "@context": SERVER_URL + "api/contexts/EntryPoint.jsonld",
       "@id": SERVER_URL + "api/",
       "@type": "EntryPoint",
-      ITEM_TYPE.lower(): "api/%s/" % (ITEM_TYPE.lower())
     }
+
+    supported_ops = gen_supported_ops(parsed_classes)
+    for op in supported_ops:
+        entrypoint_template[op.keys()[0]] = op[op.keys()[0]]
 
     return json.dumps(entrypoint_template, indent=4)
 
 
 if __name__ == "__main__":
-    print(gen_entrypoint("http://hydrus.com/", "Cots"))
+    print(gen_entrypoint("http://hydrus.com/"))
