b12bd0c21b835dc3dcbafb6ae6c76b4c64606a50
Chirag Jain <jainchg@gmail.com>
2018-02-28 13:40:06 +0530

    Initial Commits

---
+++ cli.py
@@ -0,0 +1,99 @@
+"""Demo script for setting up Hydrus with any db and any API Doc."""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker,scoped_session
+
+from hydrus.app import app_factory
+from hydrus.utils import set_session, set_doc, set_hydrus_server_url, set_api_name, set_authentication
+from hydrus.data import doc_parse
+from hydrus.hydraspec import doc_maker
+from hydrus.data.db_models import Base
+from hydrus.data.user import add_user
+from doc import doc
+from gevent.wsgi import WSGIServer
+
+import click
+
+@click.command()
+@click.option("--api", default="serverapi", help="The API Name.", type=str)
+@click.option("--authentication", default=True, help="Set authentication.", type=bool)
+@click.option("--port", default=8080, help="Tell Port on which you have to run the server.", type=int)
+
+def main(api, authentication, port):
+    # The database connection URL
+    # See http://docs.sqlalchemy.org/en/rel_1_0/core/engines.html#sqlalchemy.create_engine for more info
+    # DB_URL = 'sqlite:///database.db'
+    DB_URL = 'sqlite:///:memory:'
+
+    # Define the server URL, this is what will be displayed on the Doc
+    HYDRUS_SERVER_URL = "http://localhost:" + str(port) + "/"
+
+    # The name of the API or the EntryPoint, the api will be at http://localhost/<API_NAME>
+    API_NAME = api
+
+    # print("Setting up the database")
+    click.echo("Setting up the database")
+    # Create a connection to the database you want to use
+    engine = create_engine(DB_URL)
+
+    print("Creating models")
+    click.echo()
+    # Add the required Models to the database
+    Base.metadata.create_all(engine)
+
+    # Define the Hydra API Documentation
+    # NOTE: You can use your own API Documentation and create a HydraDoc object using doc_maker
+    #       Or you may create your own HydraDoc Documentation using doc_writer [see hydrus/hydraspec/doc_writer_sample]
+    # print("Creating the API Documentation")
+    click.echo("Creating the API Documentation")
+    apidoc = doc_maker.create_doc(doc, HYDRUS_SERVER_URL, API_NAME)
+
+    # Start a session with the DB and create all classes needed by the APIDoc
+    session = scoped_session(sessionmaker(bind=engine))
+
+    # print("Adding Classes and Properties")
+    click.echo("Adding Classes and Properties")
+    # Get all the classes from the doc
+    classes = doc_parse.get_classes(apidoc.generate())     # You can also pass dictionary defined in hydrus/hydraspec/doc_writer_sample_output.py
+
+    # Get all the properties from the classes
+    properties = doc_parse.get_all_properties(classes)
+
+    # Insert them into the database
+    doc_parse.insert_classes(classes, session)
+    doc_parse.insert_properties(properties, session)
+
+    # print("Adding authorized users")
+    click.echo("Adding authorized users")
+    add_user(id_=1, paraphrase="test", session=session)
+
+    # Insert them into the database
+    doc_parse.insert_classes(classes, session)
+    doc_parse.insert_properties(properties, session)
+
+    # print("Creating the application")
+    click.echo("Creating the application")
+    # Create a Hydrus app with the API name you want, default will be "api"
+    app = app_factory(API_NAME)
+    # Set the name of the API
+    # print("Starting the application")
+    click.echo("Starting the application")
+    with set_authentication(app, authentication):
+        # Use authentication for all requests
+        with set_api_name(app, api):
+            # Set the API Documentation
+            with set_doc(app, apidoc):
+                # Set HYDRUS_SERVER_URL
+                with set_hydrus_server_url(app, HYDRUS_SERVER_URL):
+                    # Set the Database session
+                    with set_session(app, session):
+                        # Start the Hydrus app
+                        http_server = WSGIServer(('', port), app)
+                        # print("Server running at:")
+                        click.echo("Server running at:")
+                        # print(HYDRUS_SERVER_URL + API_NAME)
+                        click.echo(HYDRUS_SERVER_URL + API_NAME)
+                        try:
+                            http_server.serve_forever()
+                        except KeyboardInterrupt:
+                            pass
