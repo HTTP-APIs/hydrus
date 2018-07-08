6e18a38af778f4764dc496715874e47d30672b38
vaibhavchellani <vaibhavchellani223@gmail.com>
2018-07-08 21:40:53 +0530

    made pep8 compliant

--- hydrus/tests/test_parser.py
+++ hydrus/tests/test_parser.py
@@ -26,53 +26,58 @@ class TestParser(unittest.TestCase):
 
     def test_generate_empty_object(self):
         object_ = openapi_parser.generate_empty_object()
-        assert type(object_["prop_definition"]) is list
-        assert type(object_["op_definition"]) is list
-        assert type(object_["class_definition"]) is type
-        assert type(object_["collection"]) is bool
+        assert isinstance(object_["prop_definition"], list)
+        assert isinstance(object_["op_definition"], list)
+        assert isinstance(object_["class_definition"], type)
+        assert isinstance(object_["collection"], bool)
 
     def test_valid_endpoint(self):
         path = 'A/B/{id}/C/D'
         result = openapi_parser.valid_endpoint(path)
         assert result is "False"
-        assert type(result) is str
+        assert isinstance(result, str)
         path = 'A/B/{id}'
         result = openapi_parser.valid_endpoint(path)
         assert result is "Collection"
-        assert type(result) is str
+        assert isinstance(result, str)
         path = 'A/B/id'
         result = openapi_parser.valid_endpoint(path)
         assert result is "True"
-        assert type(result) is str
+        assert isinstance(result, str)
+
     def test_get_class_name(self):
         path = "A/B/C/Pet"
         path_list = path.split('/')
         result = openapi_parser.get_class_name(path_list)
         assert result is path_list[3]
-        assert type(result) is str
+        assert isinstance(result, str)
+
     def test_get_data_from_location(self):
         path = '#/definitions/Order'
         path_list = path.split('/')
-        result = openapi_parser.get_data_at_location(path_list,self.doc)
+        result = openapi_parser.get_data_at_location(path_list, self.doc)
         response = self.doc["definitions"]["Order"]
         assert response is result
+
     def test_sanitise_path(self):
         path = "A/B/C/{id}"
         result = openapi_parser.sanitise_path(path)
         assert result == 'A/B/C'
-        assert type(result) is str
+        assert isinstance(result, str)
+
     def test_allow_parameter(self):
         parameter_block = self.doc["paths"]["/pet"]["post"]["parameters"][0]
         result = openapi_parser.allow_parameter(parameter_block)
         assert result is True
-        assert type(result) is bool
+        assert isinstance(result, bool)
         parameter_block = self.doc["paths"]["/pet"]["get"]["parameters"][0]
         result = openapi_parser.allow_parameter(parameter_block)
         assert result is False
-        assert type(result) is bool
+        assert isinstance(result, bool)
+
     def test_parse(self):
         result = openapi_parser.parse(self.doc)
-        assert type(result) is str
+        assert isinstance(result, str)
 
 
 if __name__ == '__main__':
