67d589eb3c200569c4b1412b487f2252306dfb53
PrajwalM2212 <prajwalmmath@gmail.com>
2018-09-14 13:13:27 +0530

    use mocks

--- hydrus/tests/test_doc_writer.py
+++ hydrus/tests/test_doc_writer.py
@@ -67,15 +67,15 @@ class TestDocWriter(unittest.TestCase):
         """
 
         hydra_entry_point_mock = mock_entry()
-        hydra_entry_point_mock.base_url = "https://hydrus.com/api"
+        hydra_entry_point_mock.base_url = "http://petstore.swagger.io/v2"
         hydra_entry_point_mock.entrypoint = "EntryPoint"
 
-        context = doc_writer.Context('https://hydrus.com/api',
+        context = doc_writer.Context('http://petstore.swagger.io/v2',
                                      entrypoint=hydra_entry_point_mock)
 
         expected_context = {
             "EntryPoint": "vocab:EntryPoint",
-            "vocab": "https://hydrus.com/api/vocab#"
+            "vocab": "http://petstore.swagger.io/v2/vocab#"
         }
         self.assertEqual(expected_context, context.generate())
 
@@ -101,10 +101,10 @@ class TestDocWriter(unittest.TestCase):
 
                 mocked_hydra_class.supportedProperty = [mocked_hydra_property]
 
-                context = doc_writer.Context('https://hydrus.com/api', class_=mocked_hydra_class)
+                context = doc_writer.Context('http://petstore.swagger.io/v2', class_=mocked_hydra_class)
 
                 expected_context = {
-                    "vocab": "https://hydrus.com/api/vocab#",
+                    "vocab": "http://petstore.swagger.io/v2/vocab#",
                     "hydra": "http://www.w3.org/ns/hydra/core#",
                     "members": "http://www.w3.org/ns/hydra/core#member",
                     "object": "http://schema.org/object",
@@ -113,6 +113,33 @@ class TestDocWriter(unittest.TestCase):
                 }
 
                 self.assertEqual(expected_context, context.generate())
+
+    @patch('hydrus.hydraspec.doc_writer.HydraClass', spec=doc_writer.HydraClass)
+    @patch('hydrus.hydraspec.doc_writer.HydraCollection', spec=doc_writer.HydraCollection)
+    def test_context_with_collection(self, hydra_class, hydra_collection):
+        """
+        Test method to test if correct context is generated when HydraCollection is passed
+
+        """
+        mocked_hydra_class = hydra_class()
+        mocked_hydra_class.id_ = "vocab:Pet"
+        mocked_hydra_class.title = "Pet"
+        mocked_hydra_class.desc = "Pet"
+
+        mocked_hydra_collection = hydra_collection()
+        mocked_hydra_collection.class_ = mocked_hydra_class
+        mocked_hydra_collection.name = mocked_hydra_class.title + "Collection"
+
+        context = doc_writer.Context('http://petstore.swagger.io/v2', collection=mocked_hydra_collection)
+        expected_context = {
+            "vocab": "http://petstore.swagger.io/v2/vocab#",
+            "hydra": "http://www.w3.org/ns/hydra/core#",
+            "members": "http://www.w3.org/ns/hydra/core#member",
+            "PetCollection": "vocab:PetCollection",
+            "Pet": "vocab:Pet"
+        }
+
+        self.assertEqual(expected_context, context.generate())
 
     if __name__ == '__main__':
         unittest.main()
