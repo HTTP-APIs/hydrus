a394c5019c26cbc1f1a02735f5dfa021a40515df
Mec-iS <tunedconsulting@gmail.com>
2019-02-21 15:21:43 +0000

    Add configuration management via conf.py

--- hydrus/app.py
+++ hydrus/app.py
@@ -1,9 +1,81 @@
 """Main route for the application"""
 
+import logging
+
+from sqlalchemy import create_engine
+
+from gevent.pywsgi import WSGIServer
+from sqlalchemy.orm import sessionmaker
+
 from hydrus.app_factory import app_factory
+from hydrus.conf import (
+    HYDRUS_SERVER_URL, API_NAME, DB_URL, APIDOC_OBJ, PORT, DEBUG)
+from hydrus.data import doc_parse
+from hydrus.data.db_models import Base
+from hydrus.data.exceptions import UserExists
+from hydrus.data.user import add_user
+from hydrus.hydraspec import doc_maker
+from hydrus.utils import (
+    set_session, set_doc, set_hydrus_server_url,
+    set_token, set_api_name, set_authentication)
 
+logger = logging.getLogger(__file__)
 
-if __name__ == "__main__":
+# TODO: loading the engine and creating the tables should be handled better
+engine = create_engine(DB_URL)
 
-    app = app_factory("api")
-    app.run(host='127.0.0.1', debug=True, port=8080)
+try:
+    Base.metadata.create_all(engine)
+except Exception:
+    pass
+
+session = sessionmaker(bind=engine)()
+
+apidoc = doc_maker.create_doc(APIDOC_OBJ, HYDRUS_SERVER_URL, API_NAME)
+
+classes = doc_parse.get_classes(apidoc.generate())
+
+# Get all the properties from the classes
+properties = doc_parse.get_all_properties(classes)
+
+# Insert them into the database
+doc_parse.insert_classes(classes, session)
+doc_parse.insert_properties(properties, session)
+
+AUTH = True
+TOKEN = True
+
+if AUTH:
+    try:
+        add_user(id_=1, paraphrase="test", session=session)
+    except UserExists:
+        pass
+# Insert them into the database
+doc_parse.insert_classes(classes, session)
+doc_parse.insert_properties(properties, session)
+
+# Create a Hydrus app with the API name you want, default will be "api"
+app = app_factory(API_NAME)
+
+with set_authentication(app, AUTH):
+    # Use authentication for all requests
+    with set_token(app, TOKEN):
+        with set_api_name(app, API_NAME):
+            # Set the API Documentation
+            with set_doc(app, apidoc):
+                # Set HYDRUS_SERVER_URL
+                with set_hydrus_server_url(app, HYDRUS_SERVER_URL):
+                    # Set the Database session
+                    with set_session(app, session):
+                        if __name__ == "__main__":
+                            # this is run only if development server is run
+                            # Set the name of the API
+                            app.run(host='127.0.0.1', debug=DEBUG, port=PORT)
+                        else:
+                            # Start the Hydrus app
+                            http_server = WSGIServer(('', PORT), app)
+                            print("running server at port", PORT)
+                            try:
+                                http_server.serve_forever()
+                            except KeyboardInterrupt:
+                                pass
--- hydrus/conf.py
+++ hydrus/conf.py
@@ -1,20 +1,62 @@
+"""
+Global variables are loaded or set here:
+    DEBUG
+    PORT
+    API_NAME
+    DB_URL
+    APIDOC_OBJ
+    HYDRUS_SERVER_URL
+"""
 import os
 import logging
 
+from os.path import abspath, dirname
+from pathlib import Path
+
+from importlib.machinery import SourceFileLoader
+
 logger = logging.getLogger(__file__)
+
+try:
+    DEBUG = bool(os.environ['DEBUG'])
+    logger.error('>>>' + str(DEBUG))
+except KeyError:
+    DEBUG = False
 
 # load form environment (as many globals as possible shall be in
 # environment configuration)
 try:
-    PORT = os.environ['PORT']
+    PORT = int(os.environ['PORT'])
+    logger.error('>>>' + str(PORT))
     API_NAME = os.environ['API_NAME']
     DB_URL = os.environ['DB_URL']
-except IndexError as e:
+except KeyError as e:
     logger.critical('PORT API_NAME DB_URL shall be defined'
                     'as environment variables')
     raise
 
-DEBUG = True
+# source for the Hydra documentation file can be defined as
+# as environment variable, otherwise it falls back to the example
+# TODO: why is this a Python module instead of being a JSON-LD file?
+cwd_path = Path(dirname(dirname(abspath(__file__))))
+try:
+    # try to load the path specified in environment variable
+    apidoc_env = os.environ['APIDOC_REL_PATH']
+    apidoc_path = cwd_path / Path(apidoc_env)
+except KeyError:
+    # if it is not set, use the example doc
+    apidoc_path = cwd_path / 'examples' / 'drones' / 'doc.py'
+
+try:
+    APIDOC_OBJ = SourceFileLoader(
+        'doc',
+        str(apidoc_path)).load_module().doc
+    logger.info('APIDOC path loaded from: {}'.format(apidoc_path))
+except FileNotFoundError:
+    logger.critical('No Hydra ApiDoc file to load has been found at {}. '
+                    'Cannot set APIDOC_OBJ'.format(apidoc_path))
+    raise
+
 HYDRUS_SERVER_URL = 'http://localhost:{}/'.format(PORT)
 
 
