5339b7d97023683949283127cefd26a0549f6167
vddesai1871 <vddesai1998@gmail.com>
2019-07-15 13:32:13 +0530

    Add tests for client controlled pagination.

--- hydrus/tests/test_app.py
+++ hydrus/tests/test_app.py
@@ -429,6 +429,54 @@ class ViewsTestCase(unittest.TestCase):
                     if mapping["property"] not in ["limit", "offset", "pageIndex"]:
                         assert mapping["property"] in class_props
 
+    def test_client_controlled_pagination(self):
+        """Test pagination controlled by client with help of pageIndex, offset and limit parameters."""
+        index = self.client.get("/{}".format(self.API_NAME))
+        assert index.status_code == 200
+        endpoints = json.loads(index.data.decode('utf-8'))
+        for endpoint in endpoints:
+            collection_name = "/".join(endpoints[endpoint].split(
+                "/{}/".format(self.API_NAME))[1:])
+            if collection_name in self.doc.collections:
+                response_get = self.client.get(endpoints[endpoint])
+                assert response_get.status_code == 200
+                response_get_data = json.loads(
+                    response_get.data.decode('utf-8'))
+                assert "search" in response_get_data
+                assert "mapping" in response_get_data["search"]
+                # Test with pageIndex and limit
+                params = {"pageIndex": 1, "limit": 2}
+                response_for_page_param = self.client.get(endpoints[endpoint], query_string= params)
+                assert response_for_page_param.status_code == 200
+                response_for_page_param_data = json.loads(
+                    response_for_page_param.data.decode('utf-8'))
+                assert "first" in response_for_page_param_data["view"]
+                assert "last" in response_for_page_param_data["view"]
+                if "next" in response_for_page_param_data["view"]:
+                    assert "pageIndex=2" in response_for_page_param_data["view"]["next"]
+                    next_response = self.client.get(response_for_page_param_data["view"]["next"])
+                    assert next_response.status_code == 200
+                    next_response_data = json.loads(
+                        next_response.data.decode('utf-8'))
+                    assert "previous" in next_response_data["view"]
+                    assert "pageIndex=1" in next_response_data["view"]["previous"]
+                    # Test with offset and limit
+                    params = {"offset": 1, "limit": 2}
+                    response_for_offset_param = self.client.get(endpoints[endpoint], query_string=params)
+                    assert response_for_offset_param.status_code == 200
+                    response_for_offset_param_data = json.loads(
+                        response_for_offset_param.data.decode('utf-8'))
+                    assert "first" in response_for_offset_param_data["view"]
+                    assert "last" in response_for_offset_param_data["view"]
+                    if "next" in response_for_offset_param_data["view"]:
+                        assert "offset=3" in response_for_offset_param_data["view"]["next"]
+                        next_response = self.client.get(response_for_offset_param_data["view"]["next"])
+                        assert next_response.status_code == 200
+                        next_response_data = json.loads(
+                            next_response.data.decode('utf-8'))
+                        assert "previous" in next_response_data["view"]
+                        assert "offset=1" in next_response_data["view"]["previous"]
+
     def test_GET_for_nested_class(self):
         index = self.client.get("/{}".format(self.API_NAME))
         assert index.status_code == 200
