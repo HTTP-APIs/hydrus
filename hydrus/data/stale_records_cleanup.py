e0e4fb6ab3e1404f44ba17d7c0cf5f81dc71ae99
vddesai1871 <vddesai1998@gmail.com>
2019-08-01 17:10:34 +0530

    Use incremental job_id instead of timestamp to clean stale records.

--- hydrus/data/stale_records_cleanup.py
+++ hydrus/data/stale_records_cleanup.py
@@ -16,17 +16,17 @@ def remove_stale_modification_records(session: scoped_session,
     timer.start()
     # Get all valid records.
     valid_records = session.query(Modification).order_by(
-        Modification.timestamp.desc()).limit(1000).all()
+        Modification.job_id.desc()).limit(1000).all()
     # If number of returned valid records is less than set limit then
     # there is nothing to clean up.
     if len(valid_records) < 1000:
         return
     else:
-        # Get the timestamp of last (oldest) valid record.
-        timestamp_of_last_valid_record = valid_records[-1].timestamp
+        # Get the job_id of last (oldest) valid record.
+        job_id_of_last_valid_record = valid_records[-1].job_id
         # Get all records which are older than the oldest valid record.
         stale_records = session.query(Modification).filter(
-            Modification.timestamp < timestamp_of_last_valid_record).all()
+            Modification.job_id < job_id_of_last_valid_record).all()
         for record in stale_records:
             session.delete(record)
         session.commit()
