0129259b951e5156413f74d77498b950aea77f50
Priyanshu Nayan <priyanshunayan456@gmail.com>
2020-02-22 21:51:46 +0530

    check in environment variables before falling back to sample doc (#447)
    add support for .py, .yaml, .jsonld in environment variable
    
    reformat code
    
    move logger info below else statement
    
    add base exception in conf.py
    
    refactor code using functions
    
    improve comments
    
    reduce line length-PEP8
    
    add Raise keyword in docstring

--- hydrus/conf.py
+++ hydrus/conf.py
@@ -6,15 +6,13 @@ Global variables are loaded or set here:
     DB_URL
     APIDOC_OBJ
     HYDRUS_SERVER_URL
+    FOUND_DOC
 """
 import os
 import logging
-
 from os.path import abspath, dirname
 from pathlib import Path
-
 from importlib.machinery import SourceFileLoader
-
 logger = logging.getLogger(__file__)
 
 try:
@@ -24,31 +22,70 @@ except KeyError:
 
 # load form environment (as many globals as possible shall be in
 # environment configuration)
+
 PORT = int(os.environ['PORT']) if 'PORT' in dict(os.environ).keys() else 8080
 API_NAME = os.environ['API_NAME'] if 'API_NAME' in dict(os.environ).keys() else 'api'
 DB_URL = os.environ['DB_URL'] if 'DB_URL' in dict(os.environ).keys() else 'sqlite:///database.db'
 
 
-# source for the Hydra documentation file can be defined as
-# as environment variable, otherwise it falls back to the example
-# TODO: why is this a Python module instead of being a JSON-LD file?
-cwd_path = Path(dirname(dirname(abspath(__file__))))
-try:
-    # try to load the path specified in environment variable
-    apidoc_env = os.environ['APIDOC_REL_PATH']
-    apidoc_path = cwd_path / Path(apidoc_env)
-except KeyError:
-    # if it is not set, use the example doc
-    apidoc_path = cwd_path / 'examples' / 'drones' / 'doc.py'
+def get_apidoc_path():
+    """
+    Get the path of the apidoc.
 
-try:
-    APIDOC_OBJ = SourceFileLoader(
-        'doc',
-        str(apidoc_path)).load_module().doc
-    logger.info('APIDOC path loaded from: {}'.format(apidoc_path))
-except FileNotFoundError:
-    logger.critical('No Hydra ApiDoc file to load has been found at {}. '
-                    'Cannot set APIDOC_OBJ'.format(apidoc_path))
-    raise
+    :return - Tuple (path, boolean). path denotes path of the apidoc.
+    If apidoc is not present at specified path then it falls back at sample apidoc.
+    boolean is true if the apidoc is present at the specified path.
+    boolean is false if sample apidoc is being used.
+    """
+    cwd_path = Path(dirname(dirname(abspath(__file__))))
+    try:
+        apidoc_env = os.environ['APIDOC_REL_PATH']
+        apidoc_path = cwd_path / Path(apidoc_env)
+        found_doc = True
+    except KeyError:
+        found_doc = False
+        apidoc_path = cwd_path / 'hydrus' / 'samples' / 'hydra_doc_sample.py'
+    return (apidoc_path, found_doc)
 
+
+def load_apidoc(path):
+    """
+    Parses docs of .jsonld, .py, .yaml format and loads apidoc from the given path.
+
+    :param path - Path for the apidoc to be loaded
+    :return - apidoc
+    :Raises:
+        FileNotFoundError: If the wrong path of hydradoc is specified.
+        BaseException: If hydradoc is specified in wrong format.
+    """
+    path = str(path)
+    try:
+        apidoc_format = path.split(".")[-1]
+        if apidoc_format == 'jsonld':
+            with open(path, 'r') as f:
+                api_doc = json.load(f)
+        elif apidoc_format == 'py':
+            api_doc = SourceFileLoader(
+                'doc', path).load_module().doc
+        elif apidoc_format == 'yaml':
+            with open(path, 'r') as stream:
+                api_doc = parse(yaml.load(stream))
+        else:
+            raise(
+                "Error - hydradoc format not supported."
+                "The supported formats are .py, .jsonld and .yaml")
+
+        logger.info('APIDOC path loaded from: {}'.format(path))
+        return api_doc
+    except FileNotFoundError:
+        logger.critical('No Hydra ApiDoc file to load has been found at {}. '
+                        'Cannot set APIDOC_OBJ'.format(path))
+        raise
+    except BaseException:
+        logger.critical("Problem parsing specified hydradoc file")
+        raise
+
+
+(path, FOUND_DOC) = get_apidoc_path()
+APIDOC_OBJ = load_apidoc(path)
 HYDRUS_SERVER_URL = 'http://localhost:{}/'.format(PORT)
