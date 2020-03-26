2df84b991d5cb75c6a9ee28bfa0770a3579db2fa
Samesh Lakhotia <samesh.lakhotia@gmail.com>
2020-03-26 12:58:05 +0530

    Migrate test suite from unittest to pytest - 4
    * Migrate the test_cli.py and test_pep8.py in /hydrus/tests
    to pytest

--- tests/test_pep8.py
+++ tests/test_pep8.py
@@ -1,34 +1,24 @@
 """Run PEP8 code format checker on all python files except samples directory."""
 
 import os
-import unittest
+from os.path import abspath, dirname
+
 import pep8
-from os.path import dirname, abspath
 
 
-class Pep8Test(unittest.TestCase):
-
-    def test_pep8(self):
-        """Test method to check PEP8 compliance over the entire project."""
-        self.file_structure = dirname(dirname(abspath(__file__)))
-        print("Testing for PEP8 compliance of python files in {}".format(
-            self.file_structure))
-        style = pep8.StyleGuide()
-        style.options.max_line_length = 100  # Set this to desired maximum line length
-        filenames = []
-        # Set this to desired folder location
-        for root, _, files in os.walk(self.file_structure):
-            python_files = [f for f in files if f.endswith(
-                '.py') and "examples" not in root]
-            for file in python_files:
-                if len(root.split('samples')) != 2:     # Ignore samples directory
-                    filename = '{0}/{1}'.format(root, file)
-                    filenames.append(filename)
-        check = style.check_files(filenames)
-        self.assertEqual(check.total_errors, 0, 'PEP8 style errors: %d' %
-                         check.total_errors)
-
-
-if __name__ == '__main__':
-    print("Starting tests ..")
-    unittest.main()
+def test_pep8():
+    """Test method to check PEP8 compliance over the entire project."""
+    file_structure = dirname(dirname(abspath(__file__)))
+    print(f"Testing for PEP8 compliance of python files in {file_structure}")
+    style = pep8.StyleGuide()
+    style.options.max_line_length = 100  # Set this to desired maximum line length
+    filenames = []
+    # Set this to desired folder location
+    for root, _, files in os.walk(file_structure):
+        python_files = [f for f in files if f.endswith('.py') and "examples" not in root]
+        for file in python_files:
+            if len(root.split('samples')) != 2:     # Ignore samples directory
+                filename = f'{root}/{file}'
+                filenames.append(filename)
+    check = style.check_files(filenames)
+    assert check.total_errors == 0, f'PEP8 style errors: check.total_errors'
