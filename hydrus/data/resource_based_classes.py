a3f5b0bd96ddfc688c228b878a7e70ab8c915db6
Samesh Lakhotia <samesh.lakhotia@gmail.com>
2020-06-23 22:45:27 +0530

    Add Foreign key links

--- hydrus/data/resource_based_classes.py
+++ hydrus/data/resource_based_classes.py
@@ -3,15 +3,16 @@ Script to generate the tables in hydrus database based on
 resources in the provided API Doc.
 """
 import uuid
+from sqlite3 import Connection as SQLite3Connection
 
 from hydra_python_core import doc_maker
-from sqlalchemy import Column, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-
 from hydrus.conf import APIDOC_OBJ
 from hydrus.data.doc_parse import get_classes
 from hydrus.data.exceptions import ClassNotFound
+from sqlalchemy import Column, ForeignKey, String, create_engine, event
+from sqlalchemy.engine import Engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 serverurl = "http://localhost"
 port = 8080
@@ -21,6 +22,15 @@ apidoc = doc_maker.create_doc(APIDOC_OBJ, HYDRUS_SERVER_URL, API_NAME)
 
 classes = get_classes(apidoc.generate())
 class_names = []
+
+
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
 
 engine = create_engine("sqlite:///database4.db")
 Base = declarative_base()
@@ -63,7 +73,33 @@ class Resource:
         }
         for supported_property in self.supported_properties:
             title = supported_property["title"]
-            attr_dict[title] = Column(String)
+            link = supported_property["property"]
+            if type(link) is not dict:
+                if "vocab:" in link:
+                    # if vocab: is in the link, it implies that the link is pointing to
+                    # another resource in the same ApiDoc, hence make it a Foreign Key
+                    # to that resource table
+                    foreign_table_name = link.split("vocab:")[1]
+                    attr_dict[title] = Column(
+                        String,
+                        ForeignKey(
+                            f"{foreign_table_name}.id",
+                            ondelete="CASCADE",
+                            onupdate="CASCADE",
+                        ),
+                    )
+                else:
+                    attr_dict[title] = Column(String)
+            else:
+                foreign_table_name = link["range"].split("vocab:")[1]
+                attr_dict[title] = Column(
+                    String,
+                    ForeignKey(
+                        f"{foreign_table_name}.id",
+                        ondelete="CASCADE",
+                        onupdate="CASCADE",
+                    ),
+                )
         return attr_dict
 
     def make_db_table(self):
@@ -71,11 +107,9 @@ class Resource:
         Resource.all_database_classes[self.name] = self.table_class
 
 
-all_database_classes = {}
-single_class = classes[2]
-resource = Resource(single_class)
-resource.make_db_table()
-# print(Resource.all_database_classes)
+# single_class = classes[6]
+# resource = Resource(single_class)
+# a = resource.get_attr_dict()
 
 
 class Object:
@@ -148,28 +182,41 @@ def update_object_from_db(object_, query_info):
     return id_
 
 
-# for single_class in classes:
-#     resource = Resource(single_class)
-#     resource.make_db_table()
+for single_class in classes:
+    resource = Resource(single_class)
+    resource.make_db_table()
 
 # print("Creating models....")
 # Base.metadata.drop_all(engine)
 # Base.metadata.create_all(engine)
 # print("Done")
 
-object_ = {
-    "@type": "Message",
-    "MessageString": "a gggg Good Message",
-}
+# object_ = {
+#     "@type": "State",
+#     "Speed": "100",
+#     "Position": "101",
+#     "Direction": "102",
+#     "Battery": "103",
+#     "SensorStatus": "104",
+#     "DroneID": "105",
+
+# }
 # data = Object(object_)
 # d = data.insert()
-query_info = {
-    "@type": "Message",
-    "id_": "007a98aa-2009-4a6b-9bc1-15bfa31027ae",
+object_ = {
+    "@type": "Command",
+    "State": "3e1d8fb4-df1a-41b6-a9b4-ded0f94dc196",
+    "DroneID": "96",
 }
+data = Object(object_)
+d = data.insert()
+# query_info = {
+#     "@type": "Message",
+#     "id_": "007a98aa-2009-4a6b-9bc1-15bfa31027ae",
+# }
 
-# d = get_object_from_db(query_info)
-# delete_object_from_db(query_info)
-d = update_object_from_db(object_, query_info)
+# # d = get_object_from_db(query_info)
+# # delete_object_from_db(query_info)
+# d = update_object_from_db(object_, query_info)
 
-print(d)
+# print(d)
