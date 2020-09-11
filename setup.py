67082fdd60b721bbbae1953f2010d028385ada7b
Priyanshu Nayan <priyanshunayan456@gmail.com>
2020-09-11 18:48:15 +0530

    update setup.py file to reflect latest pip upgrade (#500)

--- setup.py
+++ setup.py
@@ -3,16 +3,28 @@
 
 from setuptools import setup, find_packages
 
-try:  # for pip >= 10
+try:
+    # pip >=20
+    from pip._internal.network.session import PipSession
     from pip._internal.req import parse_requirements
-    from pip._internal.download import PipSession
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
-    from pip.download import PipSession
+    install_requires = parse_requirements('requirements.txt', session=PipSession())
+    dependencies = [str(package.requirement) for package in install_requires]
+except ImportError:
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
 
+    install_requires = parse_requirements('requirements.txt', session=PipSession())
+    dependencies = [str(package.req) for package in install_requires]
 
-install_requires = parse_requirements('requirements.txt', session=PipSession())
-dependencies = [str(package.req) for package in install_requires]
+for package_index in range(len(dependencies)):
+    if dependencies[package_index].startswith('git+'):
+        dependencies[package_index] = dependencies[package_index].split('=')[1]
 
 setup(name='hydrus',
       include_package_data=True,
@@ -28,8 +40,7 @@ setup(name='hydrus',
       install_requires=dependencies,
       packages=find_packages(
           exclude=['contrib', 'docs', 'tests*', 'hydrus.egg-info']),
-      package_dir={'hydrus':
-                   'hydrus'},
+      package_dir={'hydrus': 'hydrus'},
       entry_points='''
             [console_scripts]
             hydrus=cli:startserver
