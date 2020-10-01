b050a135b8a062d2d67557b9961af143fbf65d12
Priyanshu Nayan <priyanshunayan456@gmail.com>
2020-10-01 17:44:41 +0530

    add missing drone collection

--- examples/drones/doc_gen.py
+++ examples/drones/doc_gen.py
@@ -33,12 +33,13 @@ def doc_gen(API: str, BASE_URL: str) -> HydraDoc:
     state.add_supported_prop(HydraClassProp(
         "http://schema.org/identifier", "DroneID", False, False, True))
     # Operations
-    state.add_supported_op(HydraClassOp("GetState",
-                                        "GET",
-                                        None,
-                                        state.id_,
-                                        possible_status=[HydraStatus(code=404, desc="State not found"),
-                                                         HydraStatus(code=200, desc="State Returned")]))
+    state.add_supported_op(
+        HydraClassOp(
+            "GetState", "GET", None, state.id_,
+            possible_status=[HydraStatus(
+                code=404, desc="State not found"),
+                HydraStatus(
+                code=200, desc="State Returned")]))
     state_collection_manages = {
         "property": "rdfs:type",
         "object": state.id_
@@ -62,29 +63,28 @@ def doc_gen(API: str, BASE_URL: str) -> HydraDoc:
     # Operations
     # Drones will submit their state to the server at certain intervals or
     # when some event happens
-    drone.add_supported_op(HydraClassOp("SubmitDrone",
-                                        "POST",
-                                        drone.id_,
-                                        None,
-                                        possible_status=[HydraStatus(code=200, desc="Drone updated")]))
-    drone.add_supported_op(HydraClassOp("CreateDrone",
-                                        "PUT",
-                                        drone.id_,
-                                        None,
-                                        possible_status=[HydraStatus(code=200, desc="Drone added")]))
-    drone.add_supported_op(HydraClassOp("GetDrone",
-                                        "GET",
-                                        None,
-                                        drone.id_,
-                                        possible_status=[HydraStatus(code=404, desc="Drone not found"),
-                                                         HydraStatus(code=200, desc="Drone Returned")]))
+    drone.add_supported_op(
+        HydraClassOp(
+            "SubmitDrone", "POST", drone.id_, None,
+            possible_status=[HydraStatus(code=200, desc="Drone updated")]))
+    drone.add_supported_op(
+        HydraClassOp(
+            "CreateDrone", "PUT", drone.id_, None,
+            possible_status=[HydraStatus(code=200, desc="Drone added")]))
+    drone.add_supported_op(
+        HydraClassOp(
+            "GetDrone", "GET", None, drone.id_,
+            possible_status=[HydraStatus(
+                code=404, desc="Drone not found"),
+                HydraStatus(
+                code=200, desc="Drone Returned")]))
     drone_collection_manages = {
         "property": "rdfs:type",
         "object": drone.id_
     }
     drone_collection = HydraCollection(collection_name="DroneCollection",
-                                         collection_description="A collection of drones",
-                                         manages=drone_collection_manages)
+                                       collection_description="A collection of drones",
+                                       manages=drone_collection_manages)
 
     # NOTE: Commands are stored in a collection. You may GET a command or you
     # may DELETE it, there is not UPDATE.
@@ -94,24 +94,26 @@ def doc_gen(API: str, BASE_URL: str) -> HydraDoc:
     command.add_supported_prop(HydraClassProp(
         state.id_, "State", False, False, True))
     # Used by mechanics to get newly added commands
-    command.add_supported_op(HydraClassOp("GetCommand",
-                                          "GET",
-                                          None,
-                                          command.id_,
-                                          possible_status=[HydraStatus(code=404, desc="Command not found"),
-                                                           HydraStatus(code=200, desc="Command Returned")]))
+    command.add_supported_op(
+        HydraClassOp(
+            "GetCommand", "GET", None, command.id_,
+            possible_status=[
+                HydraStatus(
+                    code=404, desc="Command not found"),
+                HydraStatus(
+                    code=200, desc="Command Returned")]))
     # Used by server to add new commands
-    command.add_supported_op(HydraClassOp("AddCommand",
-                                          "PUT",
-                                          command.id_,
-                                          None,
-                                          possible_status=[HydraStatus(code=201, desc="Command added")]))
+    command.add_supported_op(
+        HydraClassOp(
+            "AddCommand", "PUT", command.id_, None,
+            possible_status=[HydraStatus(code=201, desc="Command added")]))
 
-    command.add_supported_op(HydraClassOp("DeleteCommand",
-                                          "DELETE",
-                                          None,
-                                          None,
-                                          possible_status=[HydraStatus(code=201, desc="Command deleted")]))
+    command.add_supported_op(
+        HydraClassOp(
+            "DeleteCommand", "DELETE", None, None,
+            possible_status=[
+                HydraStatus(
+                    code=201, desc="Command deleted")]))
     command_collection_manages = {
         "property": "rdfs:type",
         "object": command.id_
@@ -130,25 +132,25 @@ def doc_gen(API: str, BASE_URL: str) -> HydraDoc:
         "http://schema.org/identifier", "DroneID", False, False, True))
     datastream.add_supported_prop(HydraClassProp(
         "http://schema.org/geo", "Position", False, False, True))
-    datastream.add_supported_op(HydraClassOp("ReadDatastream",
-                                             "GET",
-                                             None,
-                                             datastream.id_,
-                                             possible_status=[HydraStatus(code=404, desc="Data not found"),
-                                             HydraStatus(code=200, desc="Data returned")]))
-    datastream.add_supported_op(HydraClassOp("UpdateDatastream",
-                                             "POST",
-                                             datastream.id_,
-                                             None,
-                                             possible_status=[HydraStatus(code=200, desc="Data updated")]))
-    datastream.add_supported_op(HydraClassOp("DeleteDatastream",
-                                             "DELETE",
-                                             None,
-                                             None,
-                                             possible_status=[HydraStatus(code=200, desc="Data deleted")]))
+    datastream.add_supported_op(
+        HydraClassOp(
+            "ReadDatastream", "GET", None, datastream.id_,
+            possible_status=[
+                HydraStatus(
+                    code=404, desc="Data not found"),
+                HydraStatus(
+                    code=200, desc="Data returned")]))
+    datastream.add_supported_op(
+        HydraClassOp(
+            "UpdateDatastream", "POST", datastream.id_, None,
+            possible_status=[HydraStatus(code=200, desc="Data updated")]))
+    datastream.add_supported_op(
+        HydraClassOp(
+            "DeleteDatastream", "DELETE", None, None,
+            possible_status=[HydraStatus(code=200, desc="Data deleted")]))
     datastream_collection_manages = {
-        "property":"rdfs:type",
-        "object" : datastream.id_
+        "property": "rdfs:type",
+        "object": datastream.id_
     }
     datastream_collection = HydraCollection(collection_name="DatastreamCollection",
                                             collection_description="A collection of datastream",
@@ -175,61 +177,67 @@ def doc_gen(API: str, BASE_URL: str) -> HydraDoc:
     log.add_supported_prop(HydraClassProp(
         command.id_, "Command", False, True, False))
     # GUI will get a certain log entry.
-    log.add_supported_op(HydraClassOp("GetLog",
-                                      "GET",
-                                      None,
-                                      log.id_,
-                                      possible_status=[HydraStatus(code=404, desc="Log entry not found"),
-                                                       HydraStatus(code=200, desc="Log entry returned")]))
-    log.add_supported_op(HydraClassOp("AddLog",
-                                      "PUT",
-                                      log.id_,
-                                      None,
-                                      possible_status=[HydraStatus(code=201, desc="Log entry created")]))
+    log.add_supported_op(
+        HydraClassOp(
+            "GetLog", "GET", None, log.id_,
+            possible_status=[
+                HydraStatus(
+                    code=404, desc="Log entry not found"),
+                HydraStatus(
+                    code=200, desc="Log entry returned")]))
+    log.add_supported_op(
+        HydraClassOp(
+            "AddLog", "PUT", log.id_, None,
+            possible_status=[HydraStatus(code=201, desc="Log entry created")]))
     log_collection_manages = {
         "property": "rdfs:type",
         "object": log.id_
     }
     log_collection = HydraCollection(collection_name="LogEntryCollection",
-                                            collection_description="A collection of logs",
-                                            manages=log_collection_manages)
+                                     collection_description="A collection of logs",
+                                     manages=log_collection_manages)
 
     # Single object representing the area of interest. No collections.
-    area = HydraClass( "Area", "Class for Area of Interest of the server", endpoint=True)
+    area = HydraClass("Area", "Class for Area of Interest of the server", endpoint=True)
     # Using two positions to have a bounding box
     area.add_supported_prop(HydraClassProp(
         "http://schema.org/geo", "TopLeft", False, False, True))
     area.add_supported_prop(HydraClassProp(
         "http://schema.org/geo", "BottomRight", False, False, True))
     # Allowing updation of the area of interest
-    area.add_supported_op(HydraClassOp("UpdateArea",
-                                       "POST",
-                                       area.id_,
-                                       None,
-                                       possible_status=[HydraStatus(code=200,
-                                                                    desc="Area of interest changed")]))
-    area.add_supported_op(HydraClassOp("GetArea",
-                                       "GET",
-                                       None,
-                                       area.id_,
-                                       possible_status=[HydraStatus(code=200, desc="Area of interest not found"),
-                                                        HydraStatus(code=200, desc="Area of interest returned")]))
+    area.add_supported_op(
+        HydraClassOp(
+            "UpdateArea", "POST", area.id_, None,
+            possible_status=[
+                HydraStatus(
+                    code=200, desc="Area of interest changed")]))
+    area.add_supported_op(
+        HydraClassOp(
+            "GetArea", "GET", None, area.id_,
+            possible_status=[
+                HydraStatus(
+                    code=200, desc="Area of interest not found"),
+                HydraStatus(
+                    code=200, desc="Area of interest returned")]))
 
     message = HydraClass("Message",
                          "Class for messages received by the GUI interface", endpoint=True)
     message.add_supported_prop(HydraClassProp(
         "http://schema.org/Text", "MessageString", False, False, True))
-    message.add_supported_op(HydraClassOp("GetMessage",
-                                          "GET",
-                                          None,
-                                          message.id_,
-                                          possible_status=[HydraStatus(code=200, desc="Message not found"),
-                                                           HydraStatus(code=200, desc="Message returned")]))
-    message.add_supported_op(HydraClassOp("DeleteMessage",
-                                          "DELETE",
-                                          None,
-                                          None,
-                                          possible_status=[HydraStatus(code=200, desc="Message deleted")]))
+    message.add_supported_op(
+        HydraClassOp(
+            "GetMessage", "GET", None, message.id_,
+            possible_status=[
+                HydraStatus(
+                    code=200, desc="Message not found"),
+                HydraStatus(
+                    code=200, desc="Message returned")]))
+    message.add_supported_op(
+        HydraClassOp(
+            "DeleteMessage", "DELETE", None, None,
+            possible_status=[
+                HydraStatus(
+                    code=200, desc="Message deleted")]))
     message_collection_manages = {
         "property": "rdfs:type",
         "object": message.id_
@@ -239,7 +247,7 @@ def doc_gen(API: str, BASE_URL: str) -> HydraDoc:
                                          manages=message_collection_manages)
 
     api_doc.add_supported_class(drone)
-    api_doc.add_supported_collection(message_collection)
+    api_doc.add_supported_collection(drone_collection)
     api_doc.add_supported_class(state)
     api_doc.add_supported_collection(state_collection)
     api_doc.add_supported_class(datastream)
