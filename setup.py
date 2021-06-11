c692c289d3fc79076a2b636ef0328f1f59d4ae82
Lorenzo (Mec-iS) <tunedconsulting@gmail.com>
2021-06-11 16:39:43 +0100

    Drop support for pip < 20.0

--- setup.py
+++ setup.py
@@ -11,17 +11,8 @@ try:
     install_requires = parse_requirements("requirements.txt", session=PipSession())
     dependencies = [str(package.requirement) for package in install_requires]
 except ImportError:
-    try:
-        # 10.0.0 <= pip <= 19.3.1
-        from pip._internal.download import PipSession
-        from pip._internal.req import parse_requirements
-    except ImportError:
-        # pip <= 9.0.3
-        from pip.download import PipSession
-        from pip.req import parse_requirements
-
-    install_requires = parse_requirements("requirements.txt", session=PipSession())
-    dependencies = [str(package.req) for package in install_requires]
+    msg = "Your pip version is out of date, please run `pip install --upgrade pip setuptools`"
+    raise ImportError(msg)
 
 for package_index in range(len(dependencies)):
     if dependencies[package_index].startswith("git+"):
